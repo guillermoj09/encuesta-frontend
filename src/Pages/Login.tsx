9760c96e5bac2e02dd31b299d516567657121abb
guillermoj09 <38594559+guillermoj09@users.noreply.github.com>
2022-05-10 01:34:34 -0400

    subiendo proyecto

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,90 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import { loginUser } from '../services/UserService';
+import Alert from 'react-bootstrap/Alert';
+
+const Login = ()  => {
+
+    const [email, setEmail ] = useState("");
+    const [password , setPassword ] = useState("");
+    const [error,  setError ] = useState("");
+    const [sendingData, setSendingData] = useState(false);
+
+    const login = async (e : React.SyntheticEvent) => {
+        e.preventDefault();
+        try{
+            setSendingData(true);
+            setError("");
+            console.log(email+password);
+            const res  = await loginUser(email,password);
+            const token = res.data.token;
+            console.log(token);
+            setSendingData(false);
+        }catch(errors : any){
+
+            if(errors.response){
+                errors.response.status === 403 && setError("No se puede iniciar Sesion con esas credenciales");    
+            }
+           
+            setSendingData(false);
+        }
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col lg ="5" md="10" sm="10" className="mx-auto">
+                    <Card className="mt-5">
+                        <Card.Body>
+                            <h4>Iniciar Sesion</h4><hr/>
+                            <Form onSubmit={login}>
+                                
+                                <Form.Group className="mb-3" controlId="email">
+                                    <Form.Label>Correo Electronico</Form.Label>
+                                    <Form.Control   
+                                    type="email" placeholder="e.g JohnDoe@email.com"
+                                    value={email}
+                                    onChange={ e => setEmail(e.target.value)}
+                                    ></Form.Control>  
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="password">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                    type="password" placeholder="********"
+                                        value={password}
+                                        onChange={ e => setPassword(e.target.value)}
+                                    ></Form.Control>
+                                    
+                                </Form.Group>
+                                <Button type="submit">
+                                    { sendingData ? <>
+                                        <Spinner as="span"animation="border"
+                                            size="sm"
+                                            role="status"
+                                            aria-hidden="true"
+                                            >
+                                            </Spinner>&nbsp;   
+                                            <span className="visually-hidden">Iniciando Sesion...</span>
+                                       
+                                     </>:
+                                    <>  Iniciar Sesion </>
+                                    }
+                                </Button>
+                            </Form>
+                             <Alert className="mt-4"  show={!!error} variant="danger">{error}</Alert>       
+
+                        </Card.Body>
+                    </Card>    
+                </Col>
+            </Row>
+        </Container>
+    )
+
+}
+export default Login;
---
+++ src/Pages/Register.tsx
@@ -0,0 +1,103 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import { registerUser } from '../services/UserService';
+import { Spinner } from 'react-bootstrap';
+
+
+const Register = ()  => {
+
+    const [name, setName ] = useState("");
+    const [email, setEmail ] = useState("");
+    const [password , setPassword ] = useState("");
+    const [errors,  setErrors ] = useState<any>({});
+    const [sendingData, setSendingData] = useState(false);
+
+    const register = async (e : React.SyntheticEvent) => {
+        e.preventDefault();
+        try{
+            setSendingData(true);
+            await registerUser(name,email,password); 
+            //redireccionar el usuario al panel
+            setSendingData(false);
+        }catch(errors : any){
+            setErrors(errors.response.data.errors);
+            setSendingData(false);
+        }
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col lg ="5" md="10" sm="10" className="mx-auto">
+                    <Card className="mt-5">
+                        <Card.Body>
+                            <h4>Crear Cuenta</h4><hr/>
+                            <Form onSubmit={register}>
+                                <Form.Group className="mb-3" controlId="name">
+                                    <Form.Label>Nombre</Form.Label>
+                                    <Form.Control 
+                                        isInvalid={!!errors?.name}
+                                        type="text" 
+                                        placeholder="e.g John Doe"
+                                        value={name}
+                                        onChange={ e => setName(e.target.value)}
+                                    ></Form.Control>
+                                    <Form.Control.Feedback type="invalid">
+                                        { errors?.name}
+
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="email">
+                                    <Form.Label>Correo Electronico</Form.Label>
+                                    <Form.Control   isInvalid={!!errors?.email} 
+                                    type="email" placeholder="e.g JohnDoe@email.com"
+                                    value={email}
+                                    onChange={ e => setEmail(e.target.value)}
+                                    ></Form.Control>
+                                    <Form.Control.Feedback type="invalid">
+                                        { errors?.email}
+
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="password">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                    isInvalid={!!errors?.password} 
+                                    type="password" placeholder="********"
+                                        value={password}
+                                        onChange={ e => setPassword(e.target.value)}
+                                    ></Form.Control>
+                                     <Form.Control.Feedback type="invalid">
+                                        { errors?.password}
+
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Button type="submit">
+                                    { sendingData ? <>
+                                        <Spinner as="span"animation="border"
+                                            size="sm"
+                                            role="status"
+                                            aria-hidden="true"
+                                            >
+                                            </Spinner>&nbsp;   
+                                            <span className="visually-hidden">Creando cuenta...</span>
+                                       
+                                     </>:
+                                    <>  Crear Cuenta </>
+                                    }
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>    
+                </Col>
+            </Row>
+        </Container>
+    )
+
+}
+export default Register;
